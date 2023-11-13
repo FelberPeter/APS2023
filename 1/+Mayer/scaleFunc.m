b64ee873041acdfa6a9eda538cb226750450f6c7
FelberPeter <105843728+FelberPeter@users.noreply.github.com>
2023-11-13 13:19:09 +0100

    ex 2 fertig
    e

--- 1/+Mayer/scaleFunc.m
+++ 1/+Mayer/scaleFunc.m
@@ -2,6 +2,6 @@ function [vTestFunction] = scaleFunc(sig,n,new_n)
 
 % For this implementation sig, n & new_n need to be row vectors
 
-    vTestFunction = sum(sig'.*signals.deltaFunction(new_n-n'));
+    vTestFunction = sum(sig'.*Felber.vDeltaFun(new_n-n'));
 
 end
